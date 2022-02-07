7399864d69e20d0d600a034179fb471c26441cce
Harish Guragol <42379451+HarishGuragol@users.noreply.github.com>
2022-02-07 15:01:52 +0530

    added side drawer

--- lib/screens/imageview.dart
+++ lib/screens/imageview.dart
@@ -13,14 +13,118 @@ class _ImageViewState extends State<ImageView> {
         preferredSize: Size(100, 80),
         child: SafeArea(
           child: Container(
-            height: 100,
+            height: 70,
             color: Colors.blue,
             child: Center(
                 child: const Text(
               'House Agreement',
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
             )),
           ),
+        ),
+      ),
+      //drawerEnableOpenDragGesture: true,
+      drawer: Drawer(
+        child: ListView(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            // DrawerHeader(
+            //   child: CircleAvatar(
+            //     child: Image.asset(''),
+            //   ),
+            // ),
+            // ignore: prefer_const_constructors
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0, bottom: 25.0),
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    'Categories',
+                    style: TextStyle(
+                        fontSize: 23.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 13.0, left: 15.0, right: 15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[600],
+                  onPrimary: Colors.white,
+                  minimumSize: Size(100, 50),
+                ),
+                onPressed: () {
+                  //Navigator.pushNamed(context, '/');
+                },
+                child: Text(
+                  'extension',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 13.0, left: 15.0, right: 15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange[600],
+                  onPrimary: Colors.white,
+                  minimumSize: Size(100, 50),
+                ),
+                onPressed: () {
+                  //Navigator.pushNamed(context, '/');
+                },
+                child: Text(
+                  'hint',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 13.0, left: 15.0, right: 15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green[600],
+                  onPrimary: Colors.white,
+                  minimumSize: Size(100, 50),
+                ),
+                onPressed: () {
+                  //Navigator.pushNamed(context, '/');
+                },
+                child: Text(
+                  'revocation',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 13.0, left: 15.0, right: 15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.indigo[500],
+                  onPrimary: Colors.white,
+                  minimumSize: Size(100, 50),
+                ),
+                onPressed: () {
+                  //Navigator.pushNamed(context, '/');
+                },
+                child: Text(
+                  'termination',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       body: Container(
